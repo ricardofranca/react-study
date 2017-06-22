5375e9830a30cd7f1cf910c002267133b6b036fb
Ricardo França <ricardodeoliveirafranca@gmail.com>
2017-06-22 11:35:23 -0300

    first commit

---
+++ server/ReactTest/ReactTest/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ReactTest.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
